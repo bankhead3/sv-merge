b895d49f92c2333d52731842aa42a1e41a0a9a1a
Armand Bankhead <abankhea@rhino03.fhcrc.org>
2023-10-12 11:02:00 -0700

    updated to support parsing of single vcf files

--- sv-merge.py
+++ sv-merge.py
@@ -9,7 +9,7 @@ from compare import compare
 from merge import merge
 
 parser = argparse.ArgumentParser(prog='merge-svs', description='Combines structural variant (SV) calls from multiple caller vcfs for a given sample.', epilog='manta and svaba currently supported')
-parser.add_argument('-v','--vcfs', help = '2 or more vcf file names separated by a comma with no spaces', required = True, dest = 'vcfs')
+parser.add_argument('-v','--vcfs', help = 'Vcf file names separated by a comma with no spaces.  2 vcfs required for comparison.', required = True, dest = 'vcfs')
 parser.add_argument('-s','--sample-name', help = 'Sample name or label that will prefix output files',required = True, dest = 'sample')
 # parser.add_argument('-t','--save-tmp-files', help = 'Y or N indicating if intermediate temp tables should saved', required = False, dest = 'save_tmp')
 parser.add_argument('-o','--outdir', help = 'Path to destination directory', required = False, dest = 'outdir')
@@ -28,14 +28,15 @@ caller_order = args.caller_order.split(',')
 
 def main():
     vcf_list = sorted(vcfs.split(','))
-    assert len(vcf_list) > 1
-
+    
     # parse vcfs
     df_all = parse_vcfs(vcf_list,out_dir,sample,verbose)
-    # compare calls
-    df_comp = compare(df_all,out_dir,sample,slack,verbose)
-    # merge calls to be 
-    merge(df_all,df_comp,out_dir,sample,verbose,caller_order)
+
+    if len(vcf_list) > 1:  # only possible to compare more than 1 vcf
+        # compare calls
+        df_comp = compare(df_all,out_dir,sample,slack,verbose)
+        # merge calls to be 
+        merge(df_all,df_comp,out_dir,sample,verbose,caller_order)
     
 if __name__ == '__main__':
     main()
